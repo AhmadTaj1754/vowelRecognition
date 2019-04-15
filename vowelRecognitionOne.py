f1d59e876dc7d554ef49deb086ebf076d7f4017f
Ahmad Tajgardoun <ahmadtaj@Ahmads-MBP.fios-router.home>
2019-04-15 09:01:47 -0400

    one function algo

---
+++ vowelRecognitionOne.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+
+
+
+
+
+
+def vowel_recognition(s):
+    vowels ="aeiouAEIOU"
+    count = 0
+
+    times= len(s)
+    x=0
+
+    for i in range(len(s)):
+            if s[i] in vowels:
+                count+=1
+
+    for j in range(times):
+        for i in range(len(s)-1):
+            check= s[0:(i+2)]
+            for i in range(len(check)):
+                if check[i] in vowels:
+                    count+=1
+        s= s[(x+1):]
+
+
+    return count
+
+
+a= vowel_recognition("bbbb")
+b= vowel_recognition("baceb")
+c= vowel_recognition("aeiou")
+d= vowel_recognition("aeiouAEIOU")
+e= vowel_recognition("Programfinishesquiggledwithexitcode1PressENTERtsquiggletconsolehedwithexitcode1PressENTEfinsquiggledwithexitcode1PressENTERtoexitconsquigglesolehe")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
+
+
+
+
+# end
